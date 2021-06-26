a6ec9aed838898373351c26ea0a6bbaaeb662d7d
Alexander Vanyukov <arcon595@gmail.com>
2021-06-27 01:12:23 +0300

    Add project

---
+++ PostgreSQLControlApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace PostgreSQLControlApp
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmMain));
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbStatus
+            // 
+            resources.ApplyResources(this.lbStatus, "lbStatus");
+            this.lbStatus.Name = "lbStatus";
+            // 
+            // btnStart
+            // 
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            resources.ApplyResources(this.btnStop, "btnStop");
+            this.btnStop.BackColor = System.Drawing.Color.Crimson;
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRestart
+            // 
+            resources.ApplyResources(this.btnRestart, "btnRestart");
+            this.btnRestart.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // fmMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "fmMain";
+            this.Load += new System.EventHandler(this.fmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ PostgreSQLControlApp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Management;
+using System.Resources;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostgreSQLControlApp
+{
+    public partial class fmMain : Form
+    {
+        public fmMain()
+        {
+            if (CultureInfo.CurrentCulture.Name == "ru-RU")
+            {
+                rs = new ResourceManager("PostgreSQLControlApp.ruResource", typeof(fmMain).Assembly);
+            }
+            else
+            {
+                rs = new ResourceManager("PostgreSQLControlApp.enResource", typeof(fmMain).Assembly);
+            }
+            InitializeComponent();
+        }
+
+        ResourceManager rs;
+        const string ServiceName = "postgresql-x64-13";
+        ServiceController postgresService;
+        string serverStopped;
+        string serverIsRunning;
+
+        private void fmMain_Load(object sender, EventArgs e)
+        {
+            serverStopped = rs.GetString("serverStopped");
+            serverIsRunning = rs.GetString("serverRunning");
+            ServiceController[] services = ServiceController.GetServices();
+            postgresService = services.Where(x => x.ServiceName == ServiceName).Single();
+            if (postgresService.Status.Equals(ServiceControllerStatus.Stopped) ||
+                postgresService.Status.Equals(ServiceControllerStatus.StopPending))
+            {
+                //if current status is "stopped"
+                lbStatus.Text = serverStopped;
+                lbStatus.ForeColor = Color.Red;
+                btnStop.Enabled = false;
+                btnRestart.Enabled = false;
+            }
+            else
+            {
+                //if current status is not "stopped"
+                lbStatus.Text = serverIsRunning;
+                lbStatus.ForeColor = Color.Green;
+                btnStart.Enabled = false;
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+            btnRestart.Enabled = false;
+            postgresService.Start();
+            while (postgresService.Status != ServiceControllerStatus.Running)
+            {
+                Thread.Sleep(1000);
+                postgresService.Refresh();
+            }
+            lbStatus.Text = serverIsRunning;
+            lbStatus.ForeColor = Color.Green;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnRestart.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+            btnRestart.Enabled = false;
+            postgresService.Stop();
+            while (postgresService.Status != ServiceControllerStatus.Stopped)
+            {
+                Thread.Sleep(1000);
+                postgresService.Refresh();
+            }
+            lbStatus.Text = serverStopped;
+            lbStatus.ForeColor = Color.Red;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnRestart.Enabled = false;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = false;
+            btnRestart.Enabled = false;
+            postgresService.Stop();
+            while (postgresService.Status != ServiceControllerStatus.Stopped)
+            {
+                Thread.Sleep(1000);
+                postgresService.Refresh();
+            }
+            lbStatus.Text = serverStopped;
+            lbStatus.ForeColor = Color.Red;
+
+            postgresService.Start();
+            while (postgresService.Status != ServiceControllerStatus.Running)
+            {
+                Thread.Sleep(1000);
+                postgresService.Refresh();
+            }
+            lbStatus.Text = serverIsRunning;
+            lbStatus.ForeColor = Color.Green;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnRestart.Enabled = true;
+        }
+    }
+}
